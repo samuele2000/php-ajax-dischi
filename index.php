5170f7591f16e7d9f90d1d6898d2cee157c03453
samuele <spadaro.samuele@gmail.com>
2022-05-09 16:44:28 +0200

    milestone 1 fatta

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php 
+    $database = [
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+            'genre' => 'Rock'
+        ],
+        [
+            'title' => 'Live at Wembley 86',
+            'author' => 'Queen',
+            'year' => 1992,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'Brave new World',
+            'author' => 'Iron Maiden',
+            'year' => 2000,
+            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+            'genre' => 'Metal'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+            'genre' => 'Rock'
+        ]
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>php Dischi</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <img src="./asset/img/Spotify_icon.svg" alt="Logo Spotify">
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                    <?php 
+                    foreach ( $database as $song ){
+                        echo "<div class='card'>";
+                        echo    '<img ' . 'src='. '"' . $song['poster'] . '"' . 'alt="">';
+                        echo    '<h3>' . $song['title'] . '</h3>';
+                        echo    '<h4>' . $song['author'] . '</h4>';
+                        echo    '<h4>' . $song['year'] . '</h4>';
+                        echo "</div>";
+                    }
+                    ?>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
